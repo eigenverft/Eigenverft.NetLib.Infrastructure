0e4b45bf4726bab60a5c5ccf552d929d450160e8
Eigenverft <eigenverft@outlook.com>
2026-09-09 17:44:17 +0200

    [2026-09-09] Remove Binding documentation template

--- src/prj/Eigenverft.NetLib.Configuration.Binding/NugetAssets/docs/js/documentation.pages.js
+++ src/prj/Eigenverft.NetLib.Configuration.Binding/NugetAssets/docs/js/documentation.pages.js
@@ -5,8 +5,7 @@
     brand: 'Eigenverft.NetLib.Configuration.Binding',
     brandIcon: 'bi-journal-text',
     pages: [
-      { path: 'index.html', label: 'Binding', title: 'Eigenverft.NetLib.Configuration.Binding', icon: 'bi-sliders' },
-      { path: 'DocTemplate.html', label: 'Template', title: 'Documentation template', icon: 'bi-file-earmark-code' }
+      { path: 'index.html', label: 'Binding', title: 'Eigenverft.NetLib.Configuration.Binding', icon: 'bi-sliders' }
     ],
     onlineLinks: []
   };
